4a31faa12275a23478b9ce10110aa07edbb067cf
nardele salomon <noliveira@telelistas.net>
2013-12-02 14:57:14 -0200

    add ds_hash tests

--- lib/ds_hash/hash_core_ext.rb
+++ lib/ds_hash/hash_core_ext.rb
@@ -1,29 +1,27 @@
 
 class Hash
   
-  unless self.method_defined? 'deep_dup'
-  	
-  	##
-  	# :method: deep_dup
-  	# Returns a deep copy of hash. like rails active support
+  ##
+  # :method: deep_dup
+  # 	Returns a deep copy of hash. like rails active support
 
-  	##  -- mantenhas este comentario senão o RDOC não processa o meto dinamico corretamente
-  	define_method 'deep_dup' do
-		    duplicate = self.dup
-		    duplicate.each_pair do |k,v|
-		      tv = duplicate[k]
-		      duplicate[k] = tv.is_a?(Hash) && v.is_a?(Hash) ? tv.deep_dup : v
-		    end
-		    duplicate
-		 	end
-	end
+  define_method 'deep_dup' do
+		duplicate = self.dup
+			duplicate.each_pair do |k,v|
+				tv = duplicate[k]
+				duplicate[k] = tv.is_a?(Hash) && v.is_a?(Hash) ? tv.deep_dup : v
+			end
+			duplicate
+	end unless self.method_defined? 'deep_dup'
 	
-	# ----self remove all empty values and/or keys
+	##
+	# self remove all empty values and/or keys
 	def compact! compact_key=true
 	  deep_swap_proc = Proc.new { |k, v| v.delete_if(&deep_swap_proc) if v.kind_of?(Hash);  v.to_s.empty? || (compact_key && k.to_s.empty? ) }
       delete_if &deep_swap_proc
 	end
 
+  ##
 	# remove all empty values and/or keys of hash
 	def compact compact_key=true
      hsh = self.deep_dup
--- lib/ds_hash/version.rb
+++ lib/ds_hash/version.rb
@@ -1,6 +1,6 @@
 module DsHash
     MAJOR = 0
-    MINOR = 1
+    MINOR = 0
     PATCH = 0
     BUILD = 'pre'
     VERSION = [MAJOR, MINOR, PATCH, BUILD].compact.join('.')
