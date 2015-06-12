83ee38a494926c2e9d43a5a7a45eb7ea13a609df
nardele salomon <noliveira@telelistas.net>
2015-06-12 14:09:08 -0300

    add array#clean

--- lib/ds_hash.rb
+++ lib/ds_hash.rb
@@ -1,5 +1,6 @@
 require 'ds_hash/version'
 require 'ds_hash/hash_core_ext'
+require 'ds_hash/array_core_ext'
 
 module DsHash
 end
---
+++ lib/ds_hash/array_core_ext.rb
@@ -0,0 +1,28 @@
+class Array
+  unless self.method_defined? 'clean'
+  
+    def clean!
+      swoop = Proc.new { |v| 
+        if v.respond_to? 'clean!'
+          v.clean!
+          false
+        elsif v.respond_to? 'compact!'
+          v.compact!
+          false
+        else
+          v.to_s.empty? 
+        end
+      }
+      delete_if &swoop
+    end
+    
+    def clean
+      arr = self.deep_dup
+      arr.clean!
+    end
+    
+    alias_method :compact, :clean unless self.method_defined? 'compact'
+    alias_method :compact!, :clean! unless self.method_defined? 'compact!'
+  end
+
+end
